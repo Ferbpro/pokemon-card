16c207866dd90e1aae4163dbfaf68a5ca41c75ba
Ferbpro <122457217+Ferbpro@users.noreply.github.com>
2023-10-05 19:09:35 +0700

    Add files via upload

---
+++ Logo.tsx
@@ -0,0 +1,11 @@
+import { Animate } from "components/Animate"
+
+const LogoComponent = () => (
+  <img
+    height={40}
+    src="https://fontmeme.com/permalink/190314/03655fc9c0c5ef371245622978eaa0a7.png"
+    alt="pokemon-go-font"
+  />
+)
+
+export const Logo = Animate(LogoComponent)
